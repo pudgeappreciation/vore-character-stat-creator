19ff0e5aaaf312637ff1fe3ba0a67e36d3410605
PudgeAppreciation <187714606+pudgeappreciation@users.noreply.github.com>
2024-11-07 23:15:36 -0500

    feature: import bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const sassSettings = {
+    verbose: false,
+    quietDeps: true,
+    includePaths: ["./node_modules/bootstrap/scss/"],
+};
+
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
+
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    ...sassSettings,
+                },
+                sass: {
+                    ...sassSettings,
+                },
+            },
+        },
+    },
+    css: ["~/assets/style/bootstrap_import.scss"],
 });
